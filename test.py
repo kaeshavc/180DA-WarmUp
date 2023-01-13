0540658e43d1e8630fea52f16e9832cded9c134b
kaeshavc <kaeshavc@gmail.com>
2023-01-12 16:46:10 -0800

    adding test.py file

---
+++ test.py
@@ -0,0 +1,9 @@
+if __name__ == 'main':
+    x = "180_DA_DB"
+    if x == "180_DA_DB":
+        print("You are living in 2023")
+    else:
+        #this is a comment
+        x = x + " - Best class ever"
+        print(x)
+    
